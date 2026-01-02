56ba692f95299f69e6ed4ff56a32d5765182a033
Babiyashini <babiyashinivaradaraj@gmail.com>
2026-01-02 11:44:24 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainningTickets int = 50
+	var bookings string
+	fmt.Printf("conferenceTickets is %T, remianingTickets is %T, conferenceName is %T\n", conferenceTickets, remainningTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainningTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainningTickets = remainningTickets - int(userTickets)
+	bookings = append(bookings, firstName+" "+lastName)
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings)
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainningTickets, conferenceName)
+}
